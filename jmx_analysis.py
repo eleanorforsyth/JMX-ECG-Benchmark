608376ee2e1fe5b5505d150c8c9b3cedacd36e40
Bernd Porr <mail@berndporr.me.uk>
2022-06-19 13:41:49 +0100

    Jitter on intervals is the same as jitter on diff anno-det
    No need to do the jitter on the differences as they cancel out
    and then also the FP,FN,... won't need subtracting one form it.
    
    Done a lot of code tidying up. Looks now very clean and tidy.

--- jmx_analysis.py
+++ jmx_analysis.py
@@ -1,37 +1,35 @@
 """
 JMX analysis
 ============
-Analyses a detector for interval
-analysis, where Jitter, Missed beats and eXtra/spurious detections are
-identified. Jitter is taken as the difference between the
-annotated interval and the detected interval.
+Analyses a detector for Jitter, Missed beats and eXtra/spurious detections.
+Missed beats and extra/spurios detections are then used to calculate precision
+as a normalised measure independent on the number of beats.
 """
 import numpy as np
 import util
 from scipy import stats
 
-a = 2 # number of annotated beats to trim from start
-b = -2 # number of annotated beats to trim from end
-
 # Used to determine how many beats could have been at max heartrate.
 # This is needed to calculate the true negatives (TN).
-# https://www.cdc.gov/physicalactivity/basics/measuring/heartrate.htm
 maxHR = 220
-
-# keys for the jmx dict:
-key_jitter = "jitter"
-key_accuracy = "accuracy"
-key_jmx = "jmx"
-key_tp = "TP"
-key_tn = "TN"
-key_fp = "FP"
-key_fn = "FN"
 
 # Cassirame et al. 2019 requires a jitter of less than 4ms for
 # a detector which can analyse HRV. With the mapping curve below
 # that gives then an 80% rating for the jitter. Of course jitter
-# of zero is best!
+# of zero is best and gives 100%.
 norm_jitter = 4E-3 # sec
+
+a = 2 # number of annotated beats to trim from start
+b = -2 # number of annotated beats to trim from end
+
+# keys for the jmx dict:
+key_jitter = "jitter" # temproal jitter in s
+key_accuracy = "accuracy" # statistical accuracy
+key_jmx = "jmx" # jmx score
+key_tp = "TP" # True positives
+key_tn = "TN" # True negatives
+key_fp = "FP" # False positives
+key_fn = "FN" # False negatives
 
 def mapping_curve():
     ratio_is_1 = 0.8 # gives 80% rating at norm_jitter
@@ -64,7 +62,7 @@ def nearest_diff(source_array, nearest_match):
     # Calculates the nearest difference between values in two arrays and saves
     # index and sample position of nearest
     diff=[] # Temporary working array for all differences
-    used_indices=[] # Arraywhich will contain only one instance of nearest matches
+    used_indices=[] # Array which will contain only one instance of nearest matches
     
     len_source_array=len(source_array)
     nearest=np.zeros((len_source_array,3)) # store nearest index and position
@@ -98,67 +96,78 @@ def evaluate(det_posn, anno_R, fs, nSamples, trim=True):
     fs: sampling rate of the ECG file
     nSamples: number of samples in the ECG file
     """
-    
+
+    # Median delay of the detection against the annotations
     delay_correction = util.calcMedianDelay(det_posn, anno_R)
-    det_posn = np.array(det_posn)-int(delay_correction) # Correction for detector delay
-                    
+
+    # Correction for detector delay
+    det_posn = np.array(det_posn)-int(delay_correction) 
+
+    # Trims 1st and last detections
     if trim==True:
         det_posn, anno_R = util.trim_after_detection(det_posn, anno_R, a, b)
-        
+
+    # Do we have enough detections?
     if len(det_posn)<=10:
         warning='WARNING: Less than ten detections'
         print(warning)
-    
+
+    # Number of annotated R peaks
     len_anno_R = len(anno_R)
 
-    all_anno = np.zeros((len_anno_R,2)) # store for nearest anno index and position
-    
-    all_anno[:, 0] = np.linspace(0, len_anno_R-1, len_anno_R)
-    all_anno[:,1] = anno_R
-    all_anno=all_anno.astype(int)
-        
+    # Number of detected R peaks
     len_det_posn = len(det_posn)
-        
-    nearest_anno, used_anno = nearest_diff(det_posn, anno_R) # return nearest anno index and position
-    nearest_det, used_det = nearest_diff(anno_R, det_posn) # return nearest anno index and position
+
+    # return nearest anno index and position
+    nearest_anno, used_anno = nearest_diff(det_posn, anno_R)
+
+    # return nearest anno index and position
+    nearest_det, used_det = nearest_diff(anno_R, det_posn) 
     
     """ MISSED BEAT IDENTIFICATION """
     # Generate array of unpaired annotated beats = missed beats
-    unused_anno=all_anno # make 'working copy'
+    # First fill up the array with all annotation positions
+    unused_anno = np.arange(len_anno_R)
+    unused_anno = unused_anno.astype(int)
     for x in reversed(used_anno): # work backwards, deleting used annotations
         index1 = x[0] # index of used anno is first in tuple
-        unused_anno = np.delete(unused_anno, index1, 0)
+        unused_anno = np.delete(unused_anno, index1)
     
     """ EXTRA BEAT - IDENTIFICATION """
     # Find 'unpaired' detections which are NOT the nearest match to annotated positions
-    extra_det_posn = det_posn # starting point - now remove any paired detection positions
-    
-    for i in range(len_det_posn-1, -1, -1): # work backwards through extra_det_posn deleting already 'paired' detections
+    # starting point - now remove any paired detection positions
+    extra_det_posn = det_posn 
+
+    # work backwards through extra_det_posn deleting already 'paired' detection
+    for i in range(len_det_posn-1, -1, -1): 
         # print(i)
         for x in used_det:
             if det_posn[i] == x[1]: # if detected position already 'used'
                 extra_det_posn = np.delete(extra_det_posn, i, 0)
           
     """ TEMPORAL ANALYSIS SECTION """
-    interval_differences_for_jitter=[]
+    differences_for_jitter=[]
         
-    for i in range(1, len(used_anno)): 
-       
-        valid_interval_anno=int(used_anno[i][1] - used_anno[i-1][1])    
-        valid_interval_det = (nearest_det[(used_anno[i][0])][1]) - (nearest_det[(used_anno[i-1][0])][1])
+    for i in range(len(used_anno)): 
+
+        # ground truth
+        valid_anno = int(used_anno[i][1])
+
+        # detection position
+        valid_det = nearest_det[(used_anno[i][0])][1]
         
-        difference = np.abs((valid_interval_det - valid_interval_anno) / fs)
-        interval_differences_for_jitter.append(difference)
+        difference = np.abs((valid_det - valid_anno) / fs)
+        differences_for_jitter.append(difference)
 
     missed_beats = unused_anno #for clarity
     extra_beats = extra_det_posn #for clarity
 
     jmx = {}
 
-    jmx[key_jitter] = stats.median_absolute_deviation(interval_differences_for_jitter)
-    fp = len_det_posn - len(interval_differences_for_jitter) - 1 # all detections - true positive = false positive
-    fn = len_anno_R - len(interval_differences_for_jitter) - 1 # all detections
-    tp = len(interval_differences_for_jitter)
+    jmx[key_jitter] = stats.median_absolute_deviation(differences_for_jitter)
+    fp = len_det_posn - len(differences_for_jitter) # all detections - true positive = false positive
+    fn = len_anno_R - len(differences_for_jitter) # all detections
+    tp = len(differences_for_jitter)
     maxBeats = nSamples / fs * maxHR / 60
     tn = maxBeats - (tp + fn + fp) # remaining samples
     jmx[key_tp] = tp
